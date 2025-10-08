fb7af637f1b61eb1e9c4a9a13695c58370a5adf7
DeeptiEmmi <emmideepti21@gmail.com>
2025-10-08 20:47:11 +0530

    Update log.py

--- log.py
+++ log.py
@@ -6,10 +6,14 @@ log_file = config["log_file"]
 if not os.path.exists("logs"):
     os.makedirs("logs")
 
+if not os.path.exists(log_file) or os.path.getsize(log_file) == 0:
+    with open(log_file, "w") as f:
+        f.write("Timestamp, CPU(%), MEM(%), DISK(%)\n")
+
 now = datetime.datetime.now()
 cpu = psutil.cpu_percent(interval=1)
 mem = psutil.virtual_memory().percent
 disk = psutil.disk_usage('/').percent
 
 with open(log_file, "a") as f:
-    f.write(f"{now}, CPU={cpu}, MEM={mem}, DISK={disk}\n")
+    f.write(f"{now}, {cpu}, {mem}, {disk}\n")
